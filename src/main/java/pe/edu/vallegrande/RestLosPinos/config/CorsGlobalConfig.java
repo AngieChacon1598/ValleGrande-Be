46be787ec27ee1cc7ded6761efa1450bebd4e65e
AngieChacon1598 <angie.chacon.a@vallegrande.edu.pe>
2025-07-03 00:32:51 -0500

    fix: permite CORS para frontend de Render (https://vallegrande-fe.onrender.com)

--- src/main/java/pe/edu/vallegrande/RestLosPinos/config/CorsGlobalConfig.java
+++ src/main/java/pe/edu/vallegrande/RestLosPinos/config/CorsGlobalConfig.java
@@ -13,7 +13,7 @@ public class CorsGlobalConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://vallegrande-fe.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
